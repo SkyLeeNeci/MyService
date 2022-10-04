dcc6dae327cf02ed3afb5815b9ce4ed85d3183c6
VladyslavKarpenko <92163391+SkyLeeNeci@users.noreply.github.com>
2022-10-04 14:59:54 +0300

    create Foreground service with broadcast receiver (problems with time display, seconds counter does not reset )

--- app/src/main/java/test/karpenko/myservice/MainActivity.kt
+++ app/src/main/java/test/karpenko/myservice/MainActivity.kt
@@ -1,12 +1,17 @@
 package test.karpenko.myservice
 
 import android.app.ActivityManager
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import test.karpenko.myservice.databinding.ActivityMainBinding
 import test.karpenko.myservice.services.MainActivityService
+import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,4 +42,37 @@ class MainActivity : AppCompatActivity() {
             .any { it.service.className == serviceClass.name }
     }
 
+    private val receiver = object : BroadcastReceiver(){
+        override fun onReceive(p0: Context?, p1: Intent?) {
+                val time = p1?.getIntExtra("TimerResult", 0)
+                binding.progress.text = time?.let { getTimeStringFromInt(it) }
+                Log.d("MainServiceTAG", time.toString())
+
+        }
+
+    }
+
+    private fun getTimeStringFromInt(time: Int): String {
+        val seconds = time / 1000
+        val minutes = seconds / 60
+        val hours = minutes / 3600
+
+        return makeTimeString(hours, minutes, seconds)
+    }
+
+    private fun makeTimeString(hours: Int, minutes: Int, seconds: Int): String {
+        return String.format("%02d:%02d:%02d", hours,minutes, seconds)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        IntentFilter("getMediaPlayerTime").also {
+            LocalBroadcastManager.getInstance(this).registerReceiver(receiver, it)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
+    }
 }
--- app/src/main/java/test/karpenko/myservice/services/MainActivityService.kt
+++ app/src/main/java/test/karpenko/myservice/services/MainActivityService.kt
@@ -1,29 +1,37 @@
 package test.karpenko.myservice.services
 
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import test.karpenko.myservice.R
+import java.util.*
 
 class MainActivityService : Service() {
 
     private var mediaPlayer: MediaPlayer? = null
+    private var timer: Timer = Timer()
 
     override fun onBind(p0: Intent?): IBinder? = null
 
     override fun onCreate() {
-        super.onCreate()
         Log.d(TAG, "onCreate")
+        showNotification()
+        startMediaPlayer()
+        super.onCreate()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, "onStartCommand")
-        Log.d(TAG, "Current thread = ${Thread.currentThread().id}")
-        if (mediaPlayer == null) {
-            startMediaPlayer()
-        }
         return super.onStartCommand(intent, flags, startId)
     }
 
@@ -33,18 +41,63 @@ class MainActivityService : Service() {
         super.onDestroy()
     }
 
-    companion object {
-        private const val TAG = "MainServiceTAG"
-    }
-
     private fun startMediaPlayer() {
-        Thread{
+        Thread {
             mediaPlayer = MediaPlayer.create(this, R.raw.test)
             mediaPlayer?.start()
+            setUpTimer()
         }.start()
+    }
+
+    private fun setUpTimer() {
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                if (mediaPlayer?.isPlaying == true) {
+                    val duration = mediaPlayer?.duration
+                    val position = mediaPlayer?.currentPosition
+                    Log.d(TAG, " Position : $position    Duration: $duration")
+                    LocalBroadcastManager.getInstance(this@MainActivityService).sendBroadcast(
+                        Intent("getMediaPlayerTime").putExtra("TimerResult", position)
+                    )
+                } else {
+                    Log.d(TAG, "ERROR")
+                }
+            }
+        }, 0, 1000)
 
     }
-    private fun stopMediaPlayer(){
+
+    private fun stopMediaPlayer() {
         mediaPlayer?.stop()
+        timer.cancel()
+        timer = Timer()
     }
+
+
+    @SuppressLint("ObsoleteSdkInt")
+    private fun showNotification() {
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Content Text")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID, "Channel if version > O",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        startForeground(NOTIFICATION_ID, notification)
+    }
+
+    companion object {
+        private const val TAG = "MainServiceTAG"
+        private const val CHANNEL_ID = "123"
+        private const val NOTIFICATION_ID = 123
+    }
+
 }
